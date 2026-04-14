fd70cb28d1f5f500a6213c072531e6a734cbcf3c
Khazz <thelenaid@gmail.com>
2026-04-14 10:33:34 +0700

    memasukkan banyak elemen pada array

--- BinarySearch_0211.cpp
+++ BinarySearch_0211.cpp
@@ -6,5 +6,12 @@ int nPanjang;
 int x;
 
 void input(){
-    
+    while (true){
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10){
+            break;
+        }
+    }
 }
