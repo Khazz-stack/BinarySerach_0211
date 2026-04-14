8b7806f875e58d90c7e5f41cab73b4ab6f4801de
Khazz <thelenaid@gmail.com>
2026-04-14 10:58:29 +0700

    looping element

--- BinarySearch_0211.cpp
+++ BinarySearch_0211.cpp
@@ -35,6 +35,18 @@ void input()
 
 void bubbleSortArray()
 {
-    
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+    }
 }
 
