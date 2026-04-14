97462c8161f7dae110a30d30ff40a0264c57d4f5
Khazz <thelenaid@gmail.com>
2026-04-14 11:12:26 +0700

    prosedure binarysearch

--- BinarySearch_0211.cpp
+++ BinarySearch_0211.cpp
@@ -53,6 +53,70 @@ void bubbleSortArray()
 
 void display()
 {
-    
+    cout << "\n=============================================\n";
+    cout << "\n====Element Array Setelah Diurutkan (ASC)====\n";
+    cout << "\n=============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n====================================\n";
+        cout << "        Pencarian Binary Search      \n";
+        cout << "====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[V] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[X] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+
+    } while (ulang == 'y' || ulang == 'Y');
+}
+
+
+
+
 
